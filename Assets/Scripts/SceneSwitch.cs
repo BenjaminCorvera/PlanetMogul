2ac19faf6d1829e3fb27b50fc3cf70f502e1f299
BenjaminCorvera <31746124+BenjaminCorvera@users.noreply.github.com>
2019-02-22 13:53:02 -0500

    Navigatable
    Stretch goal done (?)

---
+++ Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void SceneSwitcher_Options()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void SceneSwitcher_Home()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void SceneSwitcher_Map()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void SceneSwitcher_Shop()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void SceneSwitcher_MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
